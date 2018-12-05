cce50a1861aa5b258e9ed75f93589ba9795bde66
Jake Taylor <me@jaketaylor.co>
2018-12-05 09:59:39 +0000

    Add 'back' link to Movie page. Only temporary, just kept getting stuck.

--- app/app/pages/Movie/index.js
+++ app/app/pages/Movie/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Movie extends Component {
     state = {
@@ -12,6 +13,7 @@ class Movie extends Component {
     render () {
         return (
             <div>
+                <Link to="/">Back</Link>
                 <p>
                     hello
                 </p>
