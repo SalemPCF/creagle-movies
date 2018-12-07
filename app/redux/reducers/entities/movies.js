b0221a2e1d3371c62ae9131500130714cb11ee29
Jake Taylor <me@jaketaylor.co>
2018-12-07 14:39:37 +0000

    Fix bug causing any movie from a page other than page 1 to not load if going directly to single movie page

--- app/redux/reducers/entities/movies.js
+++ app/redux/reducers/entities/movies.js
@@ -1,9 +1,11 @@
 /* Relative */
 import { MOVIES } from '../../actions/movies';
+import { MOVIE } from '../../actions/movie';
 
 const moviesReducer = (state = {}, action) => {
     switch (action.type) {
         case MOVIES.LOAD.SUCCESS:
+        case MOVIE.LOAD.SUCCESS:
             return {
                 ...state,
                 ...action.payload.data.entities.movies,
