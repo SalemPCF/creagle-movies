2154a1822006ee0fd5bcd882ae5afd615d79238f
Salem <salem.cresswell@btinternet.com>
2018-12-11 22:13:21 +0000

    Moved all indexeddb logic into the withDatabase HOC

--- app/app/components/Database/index.js
+++ app/app/components/Database/index.js
@@ -3,6 +3,7 @@
 import React, { Component, createContext } from 'react';
 import idb from 'idb';
 
+import { api } from '../../../services/api';
 import propTypes from './propTypes';
 
 const DatabaseContext = createContext();
@@ -12,6 +13,7 @@ class Database extends Component {
 
     static VERSION = 1;
 
+    // Open a handle to our IndexedDB
     db = idb.open('creagle-movies', Database.VERSION, (upgradeDb) => {
         Object.entries(this.getObjectStores()).forEach(([store, options]) => {
             if (!upgradeDb.objectStoreNames.contains(store)) {
@@ -41,12 +43,61 @@ const withDatabase = Comp => class extends Component {
 
     static displayName = `withDatabase(${Component.displayName || Component.name})`;
 
+    // Get our IndexedDB handle
     getDb = () => this.context;
+
+    // Get a reference to allow us to store stuff with IndexedDB
+    getStore = async (storeName) => {
+        const db = await this.getDb();
+        const transaction = db.transaction(storeName, 'readwrite');
+        const store = transaction.objectStore(storeName);
+
+        return store;
+    }
+
+    // Store the image in our database
+    storeImage = async (movieId, base64) => {
+        const store = await this.getStore('posters_b64');
+
+        // Store the specified blob for this movieID
+        store.put(base64, movieId);
+    }
+
+    // Load our image and save it
+    loadImage = () => new Promise(async (resolve) => {
+        const { image } = this.props;
+
+        // Load the image from the server
+        const res = await api.get(image, { responseType: 'blob' });
+
+        const reader = new FileReader();
+        reader.readAsDataURL(res.data);
+
+        reader.onloadend = () => {
+            const base64 = reader.result;
+
+            // Store the retrieved image for next time
+            this.storeImage(base64);
+
+            resolve(base64);
+        };
+    })
+
+    // Get the image from our database or load it
+    getImage = async (movieId) => {
+        const store = await this.getStore('posters_b64');
+
+        // Get an image from the store for the specified movieID
+        const img = await store.get(movieId);
+
+        // If we got an image, return it. If we didn't load it.
+        return img || this.loadImage();
+    }
 
     render () {
         return (
             <Comp
-                getDb={this.getDb}
+                getImage={this.getImage}
                 {...this.props}
             />
         );
--- app/app/pages/Movies/components/Poster/index.js
+++ app/app/pages/Movies/components/Poster/index.js
@@ -1,77 +1,28 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 import { css } from 'aphrodite';
+import React from 'react';
 
 import { withDatabase } from '../../../../components/Database';
-import { api } from '../../../../../services/api';
 
 import styles from './styles';
 
 class Poster extends React.PureComponent {
     state = { url: null };
 
-    getStore = async (storeName) => {
-        const { getDb } = this.props;
-
-        const db = await getDb();
-        const transaction = db.transaction(storeName, 'readwrite');
-        const store = transaction.objectStore(storeName);
-
-        return store;
-    }
-
-    storeImage = async (base64) => {
-        const { movieId } = this.props;
-        const store = await this.getStore('posters_b64');
-
-        // Store the specified blob for this movieID
-        store.put(base64, movieId);
-    }
-
-    loadImage = () => new Promise(async (resolve) => {
-        const { image } = this.props;
-
-        // Load the image from the server
-        const res = await api.get(image, { responseType: 'blob' });
-
-        const reader = new FileReader();
-        reader.readAsDataURL(res.data);
-
-        reader.onloadend = () => {
-            const base64 = reader.result;
-
-            // Store the retrieved image for next time
-            this.storeImage(base64);
-
-            resolve(base64);
-        };
-    })
-
-    getImage = async () => {
-        const { movieId } = this.props;
-        const store = await this.getStore('posters_b64');
-
-        // Get an image from the store for the specified movieID
-        const img = await store.get(movieId);
-
-        // If we got an image, return it,
-        // else load the image
-        return img
-            ? img
-            : this.loadImage();
+    static propTypes = {
+        getImage: PropTypes.func.isRequired,
+        movieId: PropTypes.string.isRequired,
     }
 
     componentDidMount () {
+        const { getImage, movieId } = this.props;
+
         // Initiate the image get process
-        this.getImage()
+        getImage(movieId)
             // We successfully got an image URL we want to use
-            .then((url) => {
-                this.setState({ url });
-            })
+            .then(url => this.setState({ url }))
             // We didn't get an image URL - show the fallback
-            .catch(() => {
-                const url = 'resources/no-image-available.png';
-                this.setState({ url });
-            });
+            .catch(() => this.setState({ url: 'resources/no-image-available.png' }));
     }
 
     render () {
